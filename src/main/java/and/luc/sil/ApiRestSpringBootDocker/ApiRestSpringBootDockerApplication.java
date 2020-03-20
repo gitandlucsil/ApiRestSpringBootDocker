c3eddd206022782f8d134056e14421fe5c2ff2ae
gitandlucsil <andlucsil1221@gmail.com>
2020-03-20 18:07:41 -0300

    First commit, with greeting controller

---
+++ src/main/java/and/luc/sil/ApiRestSpringBootDocker/ApiRestSpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package and.luc.sil.ApiRestSpringBootDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/and/luc/sil/ApiRestSpringBootDocker/ApiRestSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package and.luc.sil.ApiRestSpringBootDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
