1f735bf5466206a2727c363a846a32f17ac7fe42
gitandlucsil <andlucsil1221@gmail.com>
2020-03-21 20:35:12 -0300

    updated anottations in controller method

--- src/main/java/and/luc/sil/ApiRestSpringBootDocker/controller/PersonController.java
+++ src/main/java/and/luc/sil/ApiRestSpringBootDocker/controller/PersonController.java
@@ -3,11 +3,14 @@ package and.luc.sil.ApiRestSpringBootDocker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import and.luc.sil.ApiRestSpringBootDocker.model.Person;
@@ -20,28 +23,29 @@ public class PersonController {
 	@Autowired
 	private PersonService personservice;
 	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value="/{id}")
 	public Person findById(@PathVariable("id") Long id) {
 		return personservice.findById(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping
 	public List<Person> findAll() {
 		return personservice.findAll();
 	}
 	
-	@RequestMapping(method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping()
 	public Person create(@RequestBody Person person) {
 		return personservice.create(person);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT,produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping()
 	public Person update(@RequestBody Person person) {
 		return personservice.update(person);
 	}	
 	
-	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
-	public void delete(@PathVariable("id") Long id) {
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 		personservice.delete(id);
+		return ResponseEntity.ok().build();
 	}
 }
