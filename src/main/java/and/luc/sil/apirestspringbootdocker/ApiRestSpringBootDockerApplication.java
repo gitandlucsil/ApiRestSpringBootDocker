9486e3c7a8caf205c418882224caa5c0c19019cd
gitandlucsil <andlucsil1221@gmail.com>
2020-04-11 14:19:21 -0300

    implemented upload and dowload files

--- src/main/java/and/luc/sil/apirestspringbootdocker/ApiRestSpringBootDockerApplication.java
+++ src/main/java/and/luc/sil/apirestspringbootdocker/ApiRestSpringBootDockerApplication.java
@@ -3,9 +3,15 @@ package and.luc.sil.apirestspringbootdocker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
+import and.luc.sil.apirestspringbootdocker.config.FileStorageConfig;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class ApiRestSpringBootDockerApplication {
